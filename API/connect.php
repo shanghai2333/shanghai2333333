b38f7f12eb3ecd5a6dcd1200032b72a6d448634d
Morningssir <1977582074@qq.com>
2017-08-20 11:59:50 +0800

    main.html finished --sunchen

--- API/connect.php
+++ API/connect.php
@@ -1,7 +1,7 @@
 <?php
 	$db_user = 'root';
 	$db_password = '';
-	$db_database = 'calendar';
+	$db_database = 'buptca';
 	$db_host = 'localhost';
 
 	try {
--- js/common.js
+++ js/common.js
@@ -11,16 +11,58 @@ $(document).ready(function(){
 		$("#showbar #main-"+selectPart[1]).addClass('selected');
 	})
 	$("#search").click(function(){
+		$("#searchbar").removeClass('hide');
 		$("#searchbar").removeClass('zoom-out-left');
 		$("#searchbar").addClass('zoom-in-right');
 		$("#showbar").removeClass('zoom-in-left');
 		$("#showbar").addClass('zoom-out-right');
+		setTimeout(function(){
+			$("#showbar").addClass('hide');
+		},500);
 	})
 	$("#close").click(function(){
+		$("#showbar").removeClass('hide');
 		$("#searchbar").addClass('zoom-out-left');
 		$("#searchbar").removeClass('zoom-in-right');
 		$("#showbar").addClass('zoom-in-left');
+		$("#showbar").removeClass('zoom-out-left');
 		$("#showbar").removeClass('zoom-out-right');
+		setTimeout(function(){
+			$("#searchbar").addClass('hide');
+		},500);
+	})
+
+	$(".main-line").click(function(){
+		$("#detailbar").removeClass('hide');
+		$("#detailbar").addClass('zoom-in-left');
+		$("#detailbar").removeClass('zoom-out-right');
+		$("#showbar").addClass('zoom-out-left');
+		$("#showbar").removeClass('zoom-in-right');
+		setTimeout(function(){
+			$("#showbar").addClass('hide');
+		},500);
+		
+	})
+	$(".nav-detail-cls").click(function(){
+		$("#detailbar").removeClass('zoom-in-left');
+		$("#detailbar").addClass('zoom-out-right');
+		$("#showbar").removeClass('zoom-in-left');
+		$("#showbar").removeClass('zoom-out-left');
+		$("#showbar").addClass('zoom-in-right');
+		$("#showbar").removeClass('hide');
+		setTimeout(function(){
+			$("#detailbar").addClass('hide');
+		},500);
+	})
+	$(".nav-detail-collect").click(function(){
+		if($(this).find("span").attr("class") == 'icon-star-empty'){
+			$(this).find("span").removeClass('icon-star-empty');
+			$(this).find("span").addClass('icon-star-full');
+		}
+		else{
+			$(this).find("span").addClass('icon-star-empty');
+			$(this).find("span").removeClass('icon-star-full');
+		}
 	})
 })
 function resize(){
